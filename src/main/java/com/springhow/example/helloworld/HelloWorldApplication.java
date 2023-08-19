09eb96b888ca869f0f09a1b4aca66a6769bf8c5d
Kiran7461 <137591175+Kiran7461@users.noreply.github.com>
2023-08-19 15:46:27 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hi, sudhakar here";
+        return "HI THIS IS KIRAN PATIL";
     }
 
 }
